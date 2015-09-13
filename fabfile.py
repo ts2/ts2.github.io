ffa5816d1c75e3c93c7f23aa86a8d73167ec049c
Pedro Morgan <pedromorgan@gmail.com>
2015-09-13 23:51:05 +0100

    stick it up and see what heappenz

--- fabfile.py
+++ fabfile.py
@@ -6,4 +6,12 @@ from fabric.api import env, local, run, cd, sudo, warn_only
 
 
 def run_www(port=8888):
+	"""Runs a local http server to view `website` """
 	local( "python -m SimpleHTTPServer %s" % (port) )
+
+def publish():
+	"""Commits local branch and send to github.io and publish stuff"""
+	local( "git commit -a -m " )
+	local( "git push origin master " )
+	
+	
